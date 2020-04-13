3543f09400c08fcb5a96447acfde2a3953873377
anusha86rains <anusha86rains@gmail.com>
2020-04-13 12:18:21 +0530

    Adding new project

---
+++ SimpleTestProject/Hooks/DriverSetup.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleTestProject.Hooks
+{
+    using BoDi;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Firefox;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class DriverSetup
+    {
+        private IObjectContainer _objectContainer;
+        public IWebDriver Driver;
+
+        public DriverSetup(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            Driver = new ChromeDriver();
+           // Driver = new FirefoxDriver();
+            _objectContainer.RegisterInstanceAs(Driver);
+        }
+
+    }
+}
+
